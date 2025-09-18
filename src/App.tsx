2358b538d937c7cc1bef5be59bb2902f9c62f2da
vShift-Kid <vshift-kid@example.com>
2025-09-18 14:36:47 -0500

    [Cursor] Fix blank page issue - remove VapiMCPConfig reference
    - Removed remaining VapiMCPConfig component reference from App.tsx
    - This was causing JavaScript error and blank page
    - Website should now load properly

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,6 @@ function App() {
             </Routes>
             <Chatbot />
             <PhoneCaller />
-            <VapiMCPConfig />
           </Suspense>
           <StickyCTA />
         </div>
