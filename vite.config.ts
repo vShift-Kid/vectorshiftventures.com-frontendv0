1c7ec31a69cd3c5e146f784e792ea5d82db3b1e8
vShift-Kid <vshift-kid@example.com>
2025-09-20 06:44:22 -0500

    Fix GitHub Pages routing: Set pathSegmentsToKeep to 0 for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vectorshiftventures/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(fileURLToPath(new URL('./src', import.meta.url))),
